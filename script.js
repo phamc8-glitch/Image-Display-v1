3a62230502377f30b5e63a855b9a1b36c8f43e7e
phamc8-glitch <phamc8@vcu.edu>
2026-06-11 14:47:53 -0400

    edit for t4 01

--- script.js
+++ script.js
@@ -28,64 +28,111 @@ const printImages = [
 let currentIndex = 0;
 
 const imageElement = document.getElementById("img-file");
-const nextButton = document.getElementById("next");
-const prevButton = document.getElementById("prev");
+const nextButtonDigital = document.getElementById("nextDigital");
+const prevButtonDigital = document.getElementById("prevDigital");
+const nextButtonPrint = document.getElementById("nextPrint");
+const prevButtonPrint = document.getElementById("prevPrint");
 
 
 // file-selection buttons
 const bme1Button = document.getElementById("BME1");
 
 bme1Button.addEventListener("click", () => {
-  let currentIndex=0
+  currentIndex=0
   imageElement.src = digitalImages[currentIndex];
+  nextButtonDigital.hidden = false;
+  prevButtonDigital.hidden = false;
+  nextButtonPrint.hidden = true;
+  prevButtonPrint.hidden = true;
+    nextButtonDigital.disabled = false;
+    prevButtonDigital.disabled = true;
+  printVer.disabled = false;
+  digitalVer.disabled = true;
 });
 
 const bme2Button = document.getElementById("BME2");
 
 bme2Button.addEventListener("click", () => {
-  let currentIndex=2
+  currentIndex=2
   imageElement.src = digitalImages[currentIndex];
+  nextButtonDigital.hidden = false;
+  prevButtonDigital.hidden = false;
+  nextButtonPrint.hidden = true;
+  prevButtonPrint.hidden = true;
+    nextButtonDigital.disabled = false;
+    prevButtonDigital.disabled = true;
+  printVer.disabled = false;
+  digitalVer.disabled = true;
 });
 
 
 // next and previous pages buttons
-nextButton.addEventListener("click", () => {
-  if (imageElement.src = printImages) {
-    currentIndex = (currentIndex + 1) % printImages.length;
-    imageElement.src = printImages[currentIndex];
-  } else {
-    currentIndex = (currentIndex + 1) % digitalImages.length;
-    imageElement.src = digitalImages[currentIndex];
-  }
-  nextButton.disabled = true;
-  prevButton.disabled = false;
+nextButtonDigital.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % digitalImages.length;
+  imageElement.src = digitalImages[currentIndex];
+  nextButtonDigital.disabled = true;
+  prevButtonDigital.disabled = false;
 });
 
-prevButton.addEventListener("click", () => {
-  if (imageElement.src = printImages) {
-    currentIndex = (currentIndex - 1) % printImages.length;
-    imageElement.src = printImages[currentIndex];
-  } else {
-    currentIndex = (currentIndex - 1) % digitalImages.length;
-    imageElement.src = digitalImages[currentIndex];
-  }
-  nextButton.disabled = false;
-  prevButton.disabled = true;
+prevButtonDigital.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1) % digitalImages.length;
+  imageElement.src = digitalImages[currentIndex];
+  nextButtonDigital.disabled = false;
+  prevButtonDigital.disabled = true;
+});
+
+nextButtonPrint.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % printImages.length;
+  imageElement.src = printImages[currentIndex];
+  nextButtonPrint.disabled = true;
+  prevButtonPrint.disabled = false;
+});
+
+prevButtonPrint.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1) % printImages.length;
+  imageElement.src = printImages[currentIndex];
+  nextButtonPrint.disabled = false;
+  prevButtonPrint.disabled = true;
 });
 
 
 // digital and print version buttons
 const printVer = document.getElementById("print-version");
 const digitalVer = document.getElementById("digital-version");
+const digitalBtn = document.getElementsByClassName("Digital-Btn")
+const printBtn = document.getElementsByClassName("Print-Btn")
 
 printVer.addEventListener("click", () => {
   imageElement.src = printImages[currentIndex];
   digitalVer.disabled = false;
   printVer.disabled = true;
+  nextButtonDigital.hidden = true;
+  prevButtonDigital.hidden = true;
+    nextButtonPrint.hidden = false;
+    prevButtonPrint.hidden = false;
+  if (currentIndex = 0,2,4,6,8) {
+    nextButtonPrint.disabled = false;
+    prevButtonPrint.disabled = true;
+  } else {
+    nextButtonPrint.disabled = true;
+    prevButtonPrint.disabled = false;
+  }
+  
 });
 
 digitalVer.addEventListener("click", () => {
   imageElement.src = digitalImages[currentIndex];
   digitalVer.disabled = true;
   printVer.disabled = false;
+  nextButtonPrint.hidden = true;
+  prevButtonPrint.hidden = true;
+    nextButtonDigital.hidden = false;
+    prevButtonDigital.hidden = false;
+  if (currentIndex = 0,2,4,6,8) {
+    nextButtonDigital.disabled = false;
+    prevButtonDigital.disabled = true;
+  } else {
+    nextButtonDigital.disabled = true;
+    prevButtonDigital.disabled = false;
+  }
 });
